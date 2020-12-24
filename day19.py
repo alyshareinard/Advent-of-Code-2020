fe0278978d93365f165ff63299351cec96b43a12
Alysha Reinard <alysha.reinard@gmail.com>
2020-12-24 09:46:06 +0100

    day 19 part2 set up but not working

--- day19.py
+++ day19.py
@@ -25,7 +25,8 @@ def reduce_ors(rule):
 #    print("rule in or", rule)
     first = True
     for item in rule:
-        if '|' in item and first == True:
+#        print("item in or", item)
+        if '|' in item  and item != '|' and first == True:
             split = item.index('|')
 #            print("split", split)
 #            print(item[:split])
@@ -50,86 +51,19 @@ def isdone(rule):
     return True
 
 
-def create_combinations_0(rules):
-    from itertools import chain
-
-    rule_dict={}
-    first_rule = '0'
-    for rule in rules:
-        num, definition = rule.split(': ')
-        if '"' in definition:
-            rule_dict[num] = definition.replace('"', '')
-        else:
-            rule_dict[num] = definition.split(' ')
-#    print('rules', rule_dict)
-
-    rules = [rule_dict[first_rule]]
-    print('starting with rule0', rules)
-    possible = []
-
-    count=0
-    done = [] #will hold all combinations that have been played out
-    while len(rules)>0:
-        #deal with ors if we have them
-        print("\nstarting while", len(rules))
-        print(rules[0])
-        print("num done", len(done))
-        for rule in rules:
-
-            if isdone(rule):
-                done.append(''.join(rule))
-                rules.remove(rule)
-            else:
-#                print("rule", rule)
-                if '|' in chain(*rule):
-                    (rule1, rule2) = reduce_ors(rule)
-                    rules.append(rule1)
-                    rules.append(rule2)
-                    rules.remove(rule)
-   
-                else:
-                    new_rule = []
-                    for item in rule:
-#                        print("item", item)
-                        if item.isnumeric(): 
-                            insert = rule_dict[item]
-                            if '|' in insert or type(insert) == str:
-                                new_rule.append(rule_dict[item])
-                            else:
-                                new_rule = new_rule + rule_dict[item]
-       
-                        else:
-                            new_rule.append(item)
-                    rules.remove(rule)
-                    rules.append(new_rule)
-#                if count<2:
-#                    again=True
-#                count+=1
-
-#        print("rules")
-#        for rule in rules:
-#            print(rule)
-
-    #now make the combos
-    combos = []
-    for rule in rules:
-#        print(rule)
-        combos.append(''.join(rule))
-#    print('combos',done)
-    return done
-
-
 def process_rule(rule, rule_dict):
     from itertools import chain
     if '|' in chain(*rule):
-
+#    if '|' in rule:
         (rule1, rule2) = reduce_ors(rule)
         return([rule1, rule2])
     else:
         new_rule = []
         for item in rule:
+#            print("ITEMS", item)
             if item.isnumeric(): 
                 insert = rule_dict[item]
+#                print("insert", insert)
                 if '|' in insert or type(insert) == str:
                     new_rule.append(rule_dict[item])
                 else:
@@ -140,23 +74,32 @@ def process_rule(rule, rule_dict):
         return([new_rule])
 
 
-cap = 100
-
-def create_combinations_0_queue(rules, longest = 100):
-
-    from collections import deque
-
+def make_rulesdict(rules):
     rule_dict={}
-    first_rule = '0'
+
     for rule in rules:
         num, definition = rule.split(': ')
         if '"' in definition:
             rule_dict[num] = definition.replace('"', '')
         else:
             rule_dict[num] = definition.split(' ')
+    return(rule_dict)
+
+def create_combinations(rule_dict, first_rule = '0', longest = 100):
+
+    from collections import deque
+
+
 #    print('rules', rule_dict)
+    print("first_rule", first_rule)
     rules = deque()
-    rules.append(rule_dict[first_rule])
+    if '|' in rule_dict[first_rule]:
+        print("first one has an or")
+        rules.append([rule_dict[first_rule]])
+    else:
+        print("no or to see here")
+        rules.append(rule_dict[first_rule])
+
     print('starting with rule0', rules)
     messcount = 0
     count=0
@@ -166,10 +109,11 @@ def create_combinations_0_queue(rules, longest = 100):
         count+=1
         if count % 1000000 == 0:
 
-            print("\nin count", len(rules))
+            print("\nin count", len(rules), len(done))
 
 
         rule = rules.pop()
+
         new_rules = process_rule(rule, rule_dict)
         for new_rule in new_rules:
             if isdone(new_rule):# and new_rule not in done:
@@ -177,12 +121,6 @@ def create_combinations_0_queue(rules, longest = 100):
             elif len(new_rule)<longest:# and new_rule not in allrules:
                 rules.insert(0, new_rule)
 
-    #now make the combos
-    combos = []
-    for rule in rules:
-#        print(rule)
-        combos.append(''.join(rule))
-#    print('combos',done)
     return done
 
 
@@ -204,7 +142,8 @@ def run_tests():
         'aaaabbb',
     ]
 
-    combos = create_combinations_0_queue(rules)
+    rule_dict = make_rulesdict(rules)
+    combos = create_combinations(rule_dict)
 
     count = 0
     for mess in messages:
@@ -214,9 +153,9 @@ def run_tests():
 
 def part1():
     (rules, messages) = read_data()
-    longest = len(max(messages, key=len))
 
-    combos = create_combinations_0_queue(rules, longest)
+    rule_dict = make_rulesdict(rules)
+    combos = create_combinations(rule_dict)
 
     count = 0
     for mess in messages:
@@ -225,21 +164,69 @@ def part1():
     print("Part 1: ", count)
 
 def part2():
-    (rules, messages) = read_data_part2()
+#    (rules, messages) = read_data_part2()
+    (rules, messages) = read_data()
+    
+    rule_dict = make_rulesdict(rules)
+
+    combos_42 = create_combinations(rule_dict, first_rule = '42')
+    combos_31 = create_combinations(rule_dict, first_rule = '31')
+
+    #create markers for rule 8 and 11
+
+
+#    temp = rule_dict['8']
+#    print("TESTING TMP", temp+['e'])
+#    rule_dict['8'] = rule_dict['8'] + ['e']
+
+#    print("TESTING", rule_dict['8'])
+#    rule_dict['11'] =  rule_dict['11'] + ['f']
+    
+#    combos = create_combinations(rule_dict)
 
     #find longest message
     longest = len(max(messages, key=len))
-    combos = create_combinations_0_queue(rules, longest)
+    print("now doing combos")
+    count = 0
 
+    combos = ['ef']
+    done = []
+
+
+    for rule42 in combos_42:
+        for rule31 in combos_31:
+            for i in range(1, 10):
+                for j in range(1, 10):
+#                    print(i, j, rule42, rule31)
+#                    print(i*rule42 + j*rule42 + j*rule31)
+                    done.append(i*rule42 + j*rule42 + j*rule31)
+#            print(len(done[-1]))
+
+
+    print(len(done))
+    for i in range(100):
+        print(done[i])
+    print("now checking combos")
+
+
+
+#    combos = create_combinations(rules, longest)
+    messages.append('abbbabbaabbbabbababaaaab')
     count = 0
     for mess in messages:
-        if mess in combos: 
+        print('mess', mess)
+        print('done', done[0])
+        if mess in done: 
+
             count+=1
+            print("found one")
     print("Part 2: ", count)
 
 def day19():
-    part1()
-    #part2()
+#    part1()
+    part2()
+
+
 
 
 
@@ -247,5 +234,5 @@ def day19():
 
 
 if __name__ == "__main__":
-    run_tests()
+#    run_tests()
     day19()
