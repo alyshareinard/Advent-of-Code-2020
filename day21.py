88f5a2b6e4e7b16658e90046c992b99461e5e86b
Alysha Reinard <alysha.reinard@gmail.com>
2021-02-01 12:07:09 +0100

    cleaned up day21 solution

--- day21.py
+++ day21.py
@@ -1,5 +1,6 @@
 import numpy as np
 import math
+from pandas.core.common import flatten
 
 def read_data():
     with open("day21.txt") as f:
@@ -15,82 +16,72 @@ def parse_allergens(foodlist):
     count = 0
     for food in foodlist:
         (ingreds, allergs) = food.replace(')', '').split(' (contains ')
-        ingreds = ingreds.split(' ')
-        allergs = allergs.split(', ')
-        foodlist_dict[count]=ingreds
-        allergen_dict[count]=allergs
+        foodlist_dict[count] = ingreds.split(' ')
+        allergen_dict[count] = allergs.split(', ')
         count+=1
     return (foodlist_dict, allergen_dict)
 
 def collect_possibilities(keys, foodlist_dict):
+    #start with all the foods in the first item, then remove those that don't appear in subsequent items
     possibilities = foodlist_dict[keys[0]]
-    print("possibilities", possibilities)
-    print('keys1 ', keys)
-    if len(keys) > 0:
-        keys = keys[1:]
-        print('keys2 ', keys)
-        for key in keys:
-            l_poss = []
-            for food in possibilities:
-                print("checking ", food)
-                if food in foodlist_dict[key]:
-                    l_poss.append(food)
-            possibilities = l_poss
-    print(possibilities)
+
+    if len(keys) == 1:
+        return possibilities
+
+    #there are multiple items - we processed the first already, so let's go through the rest
+    keys = keys[1:]
+    for key in keys:
+        l_poss = []
+        for food in possibilities:
+            if food in foodlist_dict[key]:
+                l_poss.append(food)
+        possibilities = l_poss
     return possibilities
 
                 
 def narrow_to_final(all_food_match):
-    notdone = True
+
     all_food_match_final = {}
-    while notdone:
+    while len(all_food_match)>0:
         notdone=False
         keys = list(all_food_match.keys())
         for key in keys:
-            allergens = all_food_match[key]
-            if type(allergens) !=list:
-                all_food_match_final[key] = allergens
+            foods = all_food_match[key]
+            if type(foods) !=list:
+                all_food_match_final[key] = foods
                 del all_food_match[key]
-            elif len(allergens)==1:
-                all_food_match_final[key] = allergens[0]
+            elif len(foods)==1:
+                all_food_match_final[key] = foods[0]
                 del all_food_match[key]
-            elif len(allergens)>1:
-                notdone = True
+            elif len(foods)>1:
                 done = [all_food_match_final[x] for x in all_food_match_final]
-                new_aller=[]
-                for allergen in allergens:
-                    if allergen not in done:
-                        new_aller.append(allergen)
-                all_food_match[key] = new_aller
+                new_food=[]
+                for food in foods:
+                    if food not in done:
+                        new_food.append(food)
+                all_food_match[key] = new_food
                     
             else:
                 print("something wrong here.  Allergens = ", allergens)
-    print("done? ", all_food_match_final)
     return all_food_match_final
                 
                     
 
 def match_allergens(foodlist_dict, allergen_dict):
-    print("foods\n", foodlist_dict)
-    print("allergens\n", allergen_dict)
-    allergens = []
-    for key in allergen_dict:
-        for item in allergen_dict[key]:
-            allergens.append(item)
 
-    allergens = set(allergens)
-    print(allergens)
+    allergens = set(flatten(allergen_dict.values()))
+    
     all_food_match = {}
 
+    #step through each allergen and collect all the keys that contain that allergen
     for allergen in allergens:
-        print("allergen:", allergen)
         mykeys = []
         for key in allergen_dict:
             if allergen in allergen_dict[key]:
                 mykeys.append(key)
+        #Then go collect all the possible ingredients that match that allergen
         all_food_match[allergen] = collect_possibilities(mykeys, foodlist_dict)
 
-    print(all_food_match)
     all_food_final = narrow_to_final(all_food_match)
 
     return all_food_final
@@ -107,10 +98,7 @@ def part1(foodlist):
     print("Part 2: ", ','.join(sorted_foods))
     #####
 
-    all_foods = []
-    for key in foodlist_dict:
-        for item in foodlist_dict[key]:
-            all_foods.append(item)
+    all_foods =  list(flatten(foodlist_dict.values()))
 
     all_foods_once = list(set(all_foods))
 
@@ -127,7 +115,6 @@ def run_tests():
         'trh fvjkl sbzzf mxmxvkd (contains dairy)',
         'sqjhc fvjkl (contains soy)',
         'sqjhc mxmxvkd sbzzf (contains fish)',
-
     ]
     assert(part1(foodlist))==5
 
